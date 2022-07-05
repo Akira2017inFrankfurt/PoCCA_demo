a87487a363d635ae36ac23420be6a52e77a605de
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-07-05 05:45:47 +0200

    Update cube.py

--- codes/cube.py
+++ codes/cube.py
@@ -1,27 +1,9 @@
 import numpy as np
-
-
-def fps(original, npoints=1024):
-    center_xyz = np.sum(original, 0)
-    center_xyz = center_xyz / len(original)
-    dist = np.sum((original - center_xyz) ** 2, 1)
-    farthest = np.argmax(dist)
-    distance = np.ones(len(original)) * 1e10
-    target_index = np.zeros(npoints, dtype=np.int32)
-
-    for i in range(npoints):
-        target_index[i] = farthest
-        target_point_xyz = original[target_index[i], :]
-
-        dist = np.sum((original - target_point_xyz) ** 2, 1)
-        mask = dist < distance
-        distance[mask] = dist[mask]
-        farthest = np.argmax(distance)
-
-    return original[target_index]
+from subsample_fps import fps
 
 
 def get_random_center():
+    """随机生成一个cube中心点"""
     u = np.random.uniform(-1.0, 1.0)
     theta = 2 * np.pi * np.random.uniform(0.0, 2)
     
@@ -36,6 +18,7 @@ def get_random_center():
 
 
 def point_in_cube(point_xyz, center_xyz, side_length):
+    """判断一个点是否在cube内部"""
     flag = True
     for i in range(0, len(point_xyz)):
         if abs(point_xyz[i] - center_xyz[i]) >= (side_length / 2):
@@ -45,25 +28,26 @@ def point_in_cube(point_xyz, center_xyz, side_length):
 
 
 def get_1_cube(point_set, center_xyz, side_length, npoints=1024):
-    # 现在需要从单个点云中找到8个cube出来
-    # 存储满足条件的点的索引
+    """从单个点云中，根据1个中心点找到1个cube"""
     output_samples = []
     for i in range(0, len(point_set)):
         if point_in_cube(point_set[i], center_xyz, side_length):
             output_samples.append(i)
     samples = point_set[output_samples]
-    if len(output_samples) >= npoints:
+    
+    if len(output_samples) > npoints:
         result = fps(samples, npoints)
         return result
     else:
         return get_1_cube(point_set, center_xyz, side_length + 0.2, npoints)
-
     
-def get_cubes(point_set, num_cube=8, side_length, npoints=1024):
-    # 保存结果
+    
+def get_cubes(point_set, num_cube=8, side_length=0.5, npoints=1024):
+    """从单个点云中，根据8个中心点找到8个cube"""
     result = np.ones((num_cube, npoints, 3))
+    centers = []  # record 8 cube centers
     for i in range(0, num_cube):
-        # 随机生成一个cube中心
         center = get_random_center()
+        centers.append(center)
         result[i] = get_1_cube(point_set, center, side_length, npoints)
-    return result
+    return result, centers
