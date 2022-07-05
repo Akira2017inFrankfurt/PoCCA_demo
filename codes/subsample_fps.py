9ec6c3134e86b51bf558fdd481b3f51204e0bcf8
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-07-05 05:43:38 +0200

    Update subsample_fps.py

--- codes/subsample_fps.py
+++ codes/subsample_fps.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-
 def fps(original, npoints):
+    """
+    original: numpy array [10000, 3]
+    npoints: output point number
+    return: downsampled point set [npoints, 3]
+    """
     center_xyz = np.sum(original, 0)
-    # 得到重心点的坐标
     center_xyz = center_xyz / len(original)
-    # 计算出初始的最远点
     dist = np.sum((original - center_xyz) ** 2, 1)
     farthest = np.argmax(dist)
     distance = np.ones(len(original)) * 1e10
@@ -14,10 +16,10 @@ def fps(original, npoints):
     for i in range(npoints):
         target_index[i] = farthest
         target_point_xyz = original[target_index[i], :]
-
+        
         dist = np.sum((original - target_point_xyz) ** 2, 1)
         mask = dist < distance
         distance[mask] = dist[mask]
         farthest = np.argmax(distance)
-
+    
     return original[target_index]
