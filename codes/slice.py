3ac068c80fa66a34e0040f19bdfb3d569687e23a
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-07-05 05:44:30 +0200

    Update slice.py

--- codes/slice.py
+++ codes/slice.py
@@ -1,24 +1,5 @@
 import numpy as np
-
-
-def fps(original, npoints):
-    center_xyz = np.sum(original, 0)
-    center_xyz = center_xyz / len(original)
-    dist = np.sum((original - center_xyz) ** 2, 1)
-    farthest = np.argmax(dist)
-    distance = np.ones(len(original)) * 1e10
-    target_index = np.zeros(npoints, dtype=np.int32)
-
-    for i in range(npoints):
-        target_index[i] = farthest
-        target_point_xyz = original[target_index[i], :]
-
-        dist = np.sum((original - target_point_xyz) ** 2, 1)
-        mask = dist < distance
-        distance[mask] = dist[mask]
-        farthest = np.argmax(distance)
-
-    return original[target_index]
+from subsample import fps
 
 # 3 slices per axis， 40% points of one point cloud
 def get_index(index_slice, ration_per_slice, overlap_ration, num_all_points):
